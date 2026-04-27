c2474997eaf5537cbfaa16c6bdfb6a9870f6248a
Carlos <tuemail@gmail.com>
2026-04-27 08:37:05 -0400

    fix token railway

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.use((req, res, next) => {
 
 const PORT = 3000;
 
-// CAMBIA ESTAS 4 COSAS
-const BOT_TOKEN = '';
-const CHAT_ID = '';
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 const SUPPORT_URL = 'https://t.me/tu_soporte';
 const RULES_TEXT = `📜 Reglas del sistema
 
